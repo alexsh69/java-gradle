19721fec6957a65eada4589bbcd7b2afce63884e
alexsh69 <dfx.alex@gmail.com>
2020-03-28 19:41:09 +0300

    complete poc

---
+++ src/main/java/JvmbtGradle.java
@@ -0,0 +1,11 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class JvmbtGradle {
+    private static Logger logger = LoggerFactory.getLogger(JvmbtGradle.class);
+
+    public static void main(String [] args)
+    {
+        logger.info("Info log message");
+    }
+}
